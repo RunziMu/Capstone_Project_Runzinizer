d7c7c1a8a94730c84861a8dc0e4ed92d15b99688
itseliorz <rmu6@uwo.ca>
2023-08-11 14:22:12 -0400

    Created Report Page
    Summarize total income and expense
    - minor css adjustments

--- expense_manager/src/app/components/report/report.component.ts
+++ expense_manager/src/app/components/report/report.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Iexpense } from './../../interfaces/iexpense';
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Iincome } from 'src/app/interfaces/iincome';
+import { ExpenseService } from 'src/app/services/expense.service';
+import { IncomeService } from 'src/app/services/income.service';
 
 @Component({
   selector: 'app-report',
@@ -6,5 +11,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./report.component.css']
 })
 export class ReportComponent {
-
+  @Input() income!: Iincome[];
+  @Input() expense!: Iexpense[];
+  constructor(private incomeService: IncomeService, private route: ActivatedRoute, private expenseService: ExpenseService) {
+    expenseService.getExpense().subscribe({
+      next: (results) => {
+        this.expense = results;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+    incomeService.getIncome().subscribe({
+      next: (results) => {
+        this.income = results;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
+  }
+  calculateIncomeTotal(): number {
+    return this.income.reduce((total, item) => total + item.amount, 0);
+  }
+  calculateExpenseTotal(): number {
+    return this.expense.reduce((total, item) => total + item.amount, 0);
+  }
+  calculateBalance(): number {
+    return this.income.reduce((total, item) => total + item.amount, 0) - this.expense.reduce((total, item) => total + item.amount, 0);
+  }
 }
